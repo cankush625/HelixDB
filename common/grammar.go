6efaf4f136afec0fa9526e4d5a117ccdc2ff8e6a
Ankush Chavan <41515472+cankush625@users.noreply.github.com>
2026-05-06 21:35:39 +0530

    refactor(common): move RESP type constants from resp to common package (#79)
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

---
+++ common/grammar.go
@@ -0,0 +1,8 @@
+package common
+
+// RESP data type names
+const SimpleString = "SIMPLE_STRING"
+const SimpleError = "SIMPLE_ERROR"
+const Integer = "INTEGER"
+const BulkString = "BULK_STRING"
+const Array = "ARRAY"
--- resp/grammar.go
+++ resp/grammar.go
@@ -2,18 +2,12 @@ package resp
 
 import "HelixDB/common"
 
-const SimpleString = "SIMPLE_STRING"
-const SimpleError = "SIMPLE_ERROR"
-const Integer = "INTEGER"
-const BulkString = "BULK_STRING"
-const Array = "ARRAY"
-
 var DataTypeToFirstByteMap = map[string]string{
-	"+": SimpleString,
-	"-": SimpleError,
-	":": Integer,
-	"$": BulkString,
-	"*": Array,
+	"+": common.SimpleString,
+	"-": common.SimpleError,
+	":": common.Integer,
+	"$": common.BulkString,
+	"*": common.Array,
 }
 
 var FirstByteToDataTypeMap = common.ReverseMap(DataTypeToFirstByteMap)
